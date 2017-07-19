fd14453aec3d52e2a4ac403d285e76132907c1e0
Zsolt Makrai <mecrai@gmail.com>
2017-07-19 09:15:52 +0200

    refactor(mock decorator): remove useless data 💾

--- mock-decorator.js
+++ mock-decorator.js
@@ -60,15 +60,14 @@ let roomSample = {
 const pg = require('pg');
 require('dotenv').config()
 
-
 const config = {
-  user: process.env.User, //env var: PGUSER 
-  database: process.env.Database, //env var: PGDATABASE 
-  password: process.env.Password, //env var: PGPASSWORD 
-  host: process.env.Host, // Server hosting the postgres database 
-  port: process.env.Dataport, //env var: PGPORT 
-  max: 10, // max number of clients in the pool 
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+  user: process.env.User, //env var: PGUSER
+  database: process.env.Database, //env var: PGDATABASE
+  password: process.env.Password, //env var: PGPASSWORD
+  host: process.env.Host, // Server hosting the postgres database
+  port: process.env.Dataport, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
 const pool = new pg.Pool(config);
@@ -85,18 +84,17 @@ module.exports.query = function (text, values, callback) {
 const createHotels = require('./hotel-generator')
 
 const MockServer = function(app) {
-  
-  const setHotel = createHotels()
-  const hotel1 = setHotel.setAttributes()
-  const hotel2 = setHotel.setAttributes()
-  const hotel3 = setHotel.setAttributes()
-  const hotel4 = setHotel.setAttributes()
-  const hotel5 = setHotel.setAttributes()
-  const hotel6 = setHotel.setAttributes()
-  const hotel7 = setHotel.setAttributes()
-  // ^^ why no for loop tho? (Tibi)
-  
-  const path = require('path');
+
+    const setHotel = createHotels()
+    const hotel1 = setHotel.setAttributes()
+    const hotel2 = setHotel.setAttributes()
+    const hotel3 = setHotel.setAttributes()
+    const hotel4 = setHotel.setAttributes()
+    const hotel5 = setHotel.setAttributes()
+    const hotel6 = setHotel.setAttributes()
+    const hotel7 = setHotel.setAttributes()
+
+    const path = require('path');
 
 //-------------------------
 //----- Admin section -----
@@ -132,7 +130,7 @@ const MockServer = function(app) {
            }
        }
     };
-  
+
     const invalidResponse = {
         errors: [{
             status: 400,
@@ -152,116 +150,6 @@ const MockServer = function(app) {
             }
         }
     };
-  
-    let hotelResponse = {
-        links: {
-            self: 'https://two-ferns.glitch.me/hotels/'
-        },
-        data: [{
-            type: 'hotels',
-            id: '1',
-            attributes: {
-                location: 'Bone City',
-                name: 'Dog Heaven',
-                main_image_src: 'http://placebacon.net/300/300?image=1',
-                has_wifi: true,
-                has_parking: false,
-                has_pets: true,
-                has_restaurant: false,
-                has_bar: false,
-                has_swimming_pool: false,
-                has_air_conditioning: false,
-                has_gym: true,
-                meal_plan: 'continental plan',
-                user_id: '1',
-                booking_id: '1',
-                amount: '50',
-                currency: 'USD',
-                status: 'pending',
-                stars: '3'
-            }
-        }, {
-            type: 'hotels',
-            id: '2',
-            attributes: {
-                location: 'near Sirius',
-                name: 'Space Hotel',
-                main_image_src: 'https://placebear.com/300/300',
-                has_wifi: true,
-                has_parking: true,
-                has_pets: true,
-                has_restaurant: true,
-                has_bar: true,
-                has_swimming_pool: false,
-                has_air_conditioning: true,
-                has_gym: true,
-                meal_plan: 'continental plan',
-                user_id: '2',
-                booking_id: '1',
-                amount: '50',
-                currency: 'USD',
-                status: 'pending',
-                stars: '5'
-            }
-        }, {
-            type: 'hotels',
-            id: '3',
-            attributes: {
-                location: 'Berlin',
-                name: 'Hotel Kaiser Wilhelm II.',
-                main_image_src: 'http://placebacon.net/300/300?image=1',
-                has_wifi: false,
-                has_parking: true,
-                has_pets: false,
-                has_restaurant: true,
-                has_bar: false,
-                has_swimming_pool: false,
-                has_air_conditioning: false,
-                has_gym: true,
-                meal_plan: '',
-                user_id: '2',
-                booking_id: '1',
-                amount: '50',
-                currency: 'USD',
-                status: 'pending',
-                stars: '2'
-            }
-        }, {
-            type: 'hotels',
-            id: '4',
-            attributes: hotel1
-        }, {
-            type: 'hotels',
-            id: '5',
-            attributes: hotel2
-        }, {
-            type: 'hotels',
-            id: '6',
-            attributes: hotel3
-        }, {
-            type: 'hotels',
-            id: '7',
-            attributes: hotel4
-        }, {
-            type: 'hotels',
-            id: '8',
-            attributes: hotel5
-        }, {
-            type: 'hotels',
-            id: '9',
-            attributes: hotel6
-        }, {
-            type: 'hotels',
-            id: '10',
-            attributes: hotel7
-        }]
-    };
-  
-    let rowHotel = {
-      links: {
-            self: 'https://two-ferns.glitch.me/hotels/'
-      }
-    }
 
     const hotelError = {
         errors: [{
@@ -270,7 +158,7 @@ const MockServer = function(app) {
             detail: 'No hotels found by id: 1'
         }]
     }
-    
+
     const roomError = {
         errors: [{
             status: '404',
@@ -278,43 +166,7 @@ const MockServer = function(app) {
             detail: 'No room found by id: 1'
         }]
     }
-    
-    const roomResponse = {
-        links: {
-            self: 'https://two-ferns.glitch.me/hotels/:id/relationships/rooms'
-        },  
-        data: [{
-            type: 'rooms',
-            id: '1',
-            attributes: {
-                price: '12',
-                currency: 'EUR',
-                room_name: 'Owen Lars',
-                description: "That malfunctioning little twerp. This is all his fault! He tricked me into going this way, but he'll do no better. Wait, what's that? A transport! I'm saved! Over here! Help! Please, help! Artoo-Detoo! It's you! It's you!",
-                max_occupancy: '2'
-            }
-        }, {
-            type: 'rooms',
-            id: '2',
-            attributes: {
-                price: '10',
-                currency: 'Credit',
-                room_name: 'Greedo',
-                description: "Fighters coming in. There's too many of them! Accelerate to attack speed! Draw their fire away from the cruisers. Copy, Gold Leader.",
-                max_occupancy: '4'
-            }
-        }, {
-            type: 'rooms',
-            id: '3',
-            attributes: {
-                price: '8',
-                currency: 'Credit',
-                room_name: 'Lando Carlissian',
-                description: "Well, wait. This is interesting. Lando. Lando system? Lando's not a system, he's a man. Lando Calrissian. He's a card player, gambler, scoundrel. You'd like him. Thanks.",
-                max_occupancy: '2'
-            }
-        }]
-    }
+
 
     //----------USER FRONTEND NEW ENDPOINTS------------------
     app.get('/testhotels/', (req, res) => { //works
@@ -322,7 +174,7 @@ const MockServer = function(app) {
             res.json(result.rows)
         })
     });
-    
+
     app.get('/testrooms/', (req, res) => { //works
         pool.query('SELECT * FROM ' + rooms, function(err, result) {
             res.json(result.rows)
@@ -387,7 +239,7 @@ const MockServer = function(app) {
     });
 
 	app.get('/mapsearch', (req, res) => {
-        pool.query('SELECT * FROM ' + hotels, function(err, result) {
+        pool.query('SELECT * FROM ' + hotels, (err, result) => {
 			if(err) {
                 res.json({
                   'error': err.message
@@ -436,9 +288,9 @@ const MockServer = function(app) {
 							}
 						}
 					responseObject.data.push(featureData)
-					}			
-				})	
-				res.json(responseObject)	
+					}
+				})
+				res.json(responseObject)
 			}
 		})
     });
@@ -530,10 +382,9 @@ const MockServer = function(app) {
 				})
 				res.json(responseObject)
 			}
-				
+
 		})
 	})
-
 
     //--------------------------------------------------------
 
@@ -560,33 +411,30 @@ const MockServer = function(app) {
         })
     });
 
-
-  //
-
     app.post('/api/register/', (req, res) => {
         const email = req.body.data.attributes.email;
         const password = req.body.data.attributes.password;
         pool.query('SELECT * FROM ' + users + ' WHERE email = $1', [email], function(err, result) {
-          if(err) {
-              res.json({
+            if(err) {
+                res.json({
                 'error': err.message
-              });
-          } else {
-              if(!result.rows[0]) {
-                  pool.query('INSERT INTO ' + users + '(email, password) VALUES($1, $2) RETURNING *', [email, password], function(err, result) {
-                      if(err) {
-                          res.json({
+                });
+            } else {
+                if(!result.rows[0]) {
+                    pool.query('INSERT INTO ' + users + '(email, password) VALUES($1, $2) RETURNING *', [email, password], function(err, result) {
+                        if(err) {
+                            res.json({
                             'error': err.message
-                          });
-                      } else {
+                            });
+                        } else {
                             validResponse.data.attributes.id = result.rows[0].id;
                             validResponse.data.attributes.email = result.rows[0].email;
                             validResponse.data.attributes.password = result.rows[0].password;
-                          res.json(validResponse);
-                      }
-                  });
-              }
-          }
+                            res.json(validResponse);
+                        }
+                    });
+                }
+            }
         })
     });
 
@@ -602,12 +450,12 @@ const MockServer = function(app) {
                     commonHotels.data[i].type = result.rows[i].type;
                     commonHotels.data[i].attributes = Object.assign(result.rows[i])
                 }
-                res.send(commonHotels);                  
+                res.send(commonHotels);
             }
         });
-    });  
+    });
 
-  
+
   // NORMA FRONTEND NOT WORKING
     app.post('/api/hotels/', (req, res) => {
         const has_wifi = req.body.data.attributes.has_wifi;
@@ -642,7 +490,7 @@ const MockServer = function(app) {
          res.status(201).send(hotelResponse);
         }
     });
-  
+
     app.get('/api/hotels/:id', (req, res) => {
         const hotelID = req.params.id;
         let response = {data:{}};
@@ -654,18 +502,18 @@ const MockServer = function(app) {
                 response.data.hotel_id = result.rows[0].hotel_id;
                 response.data.type = result.rows[0].type;
                 response.data.attributes = Object.assign(result.rows[0])
-                res.send(response);                  
+                res.send(response);
             }
         });
     });
-  
+
      app.delete('/api/hotels/:id', (req, res) => {
         const hotelID = req.params.id;
         pool.query('DELETE FROM ' + hotels +' WHERE hotel_id = $1', [hotelID]), function(err, result) {
             if(err) {
                 res.json({
                 'error': err.message
-                }); 
+                });
             } else {
                 res.json({
                     'status': 'ok'
@@ -692,12 +540,12 @@ const MockServer = function(app) {
         const stars = req.body.data.attributes.stars;
         const location = req.body.data.attributes.location;
         const main_image_src = req.body.data.attributes.main_image_src;
-        let response = {data:{}};        
+        let response = {data:{}};
         const columns = 'has_wifi = $1, has_parking = $2, has_pets = $3, has_restaurant = $4, has_bar = $5, has_swimming_pool = $6, has_air_conditioning = $7, has_gym = $8, name = $9, meal_plan = $10, stars = $11, location = $12, main_image_src = $13 ';
         //const values = ' VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)';
         const valueList = [has_wifi, has_parking, has_pets, has_restaurant, has_bar, has_swimming_pool, has_air_condition, has_gym, name, meal_plan, stars, location, main_image_src, hotelID]
         // pool.query('UPDATE ' + hotels + 'SET ' + columns + ' RETURNING *', valueList, function(err, result) {
-        pool.query('UPDATE ' + hotels + ' SET '+ columns  + ' WHERE hotel_id = $14', valueList, function(err, result) {          
+        pool.query('UPDATE ' + hotels + ' SET '+ columns  + ' WHERE hotel_id = $14', valueList, function(err, result) {
             if(err) {
                 res.json({
                     'error': err.message
@@ -709,11 +557,11 @@ const MockServer = function(app) {
             }
         });
     });
-  
+
     app.post('/api/hotels/:hotelId/relationships/rooms', (req, res) => {
         res.status(201).send(req.body);
     })
-  
+
     app.get('/api/hotels/:hotelId/relationships/rooms', (req, res) => {
         commonRooms.data = [];
         pool.query('SELECT * FROM ' + rooms, function(err, result) {
@@ -731,7 +579,7 @@ const MockServer = function(app) {
             }
         });
     });
-  
+
     app.delete('/api/hotels/:hotelId/relationships/rooms/:roomId', (req, res) => {
         const roomID = req.params.roomId;
         roomResponse.data.forEach((room, index) => {
@@ -741,7 +589,7 @@ const MockServer = function(app) {
             }
         });
         res.status(404).send(roomError)
-    });  
+    });
 };
 
 module.exports = MockServer;
